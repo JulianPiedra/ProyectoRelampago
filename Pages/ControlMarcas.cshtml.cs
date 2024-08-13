77ef43af20a3c07bd0fdfce5456a3e53101e7606
TamaraChavez <tamara33chavez@gmail.com>
2024-08-13 14:47:18 -0600

    cambios

---
+++ Pages/ControlMarcas.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProyectoRelampago.Models;
+namespace ProyectoRelampago.Pages
+{
+    public class ControlMarcasModel : PageModel
+    {
+
+    }
+}
